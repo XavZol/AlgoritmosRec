28f764d100364cbbd45e7bb0ccd4bbe6b2cf78c4
XavZol <alansolis707@gmail.com>
2025-11-09 10:20:07 -0600

    Algoritmo de problema de las N-Reinas

--- algorpro.cpp
+++ algorpro.cpp
@@ -1,76 +1,104 @@
-/*Salto del Caballo */
+/*Problema de las N-Reinas */
 #include <iostream>
 #include <conio.h>
 using namespace std;
 
+//Prototipo de funciones
+void ponerReina(int, bool&);
+bool valido(int);
+
+void mostrarTablero();
+
 const int N = 8;
-int tablero[N][N];
+int reinas[N];
 
-//Desplazamientos relativos del caballo
-int d[8][2] = {{2, 1}, {1, 2}, {-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}, {1, -2}, {2, -1}};
+int main(){
+    bool solucion;
 
-void escribirTablero(){
-    int i, j;
+    ponerReina(0, solucion);
 
-    for(i=0;i<N;i++){
-        for(j=0;j<N;j++){
+    if(solucion){
+        cout<<"Si existe combinacion de reinas"<<endl;
+        mostrarTablero();
+    }
+    else{
+        cout<<"No existe combinacion de reinas"<<endl;
+    }
+
+    return 0;
+}
+
+void ponerReina(int i, bool &solucion){
+    int k = 0; //inicializa contador de movimientos
+    solucion = false;
+
+    do{
+        reinas[i] = k; //Coloca y anota reina i en la fila k
+        k++;
+
+        mostrarTablero();
+        cout<<endl;
+
+        if(valido(i)){
+            if(i<(N-1)){
+                ponerReina(i+1, solucion);
+
+                //vuelta atras
+                if(!solucion){
+                    reinas[i] = 0;
+                }
+            }
+            else{ //Todas las reinas colocadas
+                solucion = true;
+            }
+        }
+    }while(!solucion && (k<N));
+}
+bool valido(int i){
+    /*Comprobar si la reina de la columna i no se amenaza con ninguna reina ya puesta*/
+
+    bool v = true;
+
+    for(int r = 0; r<i; r++){
+        v = v && (reinas[i] != reinas[r]); //Comprobar si la fila esta vacía.
+        v = v && ((reinas[i]-i) != (reinas[r]-r)); //Diagonal 1
+        v = v && ((reinas[i]+i) != (reinas[r]+r)); //Diagonal 2
+    }
+
+    return v;
+}
+
+void mostrarTablero(){
+    int tablero[N][N];
+
+    //Asignamos los valores del tablero a 0
+    for(int i = 0; i<N; i++){
+        for(int j = 0; j<N; j++){
+            tablero[i][j] = 0;
+        }
+    }
+
+    cout<<"\nMostrando arreglo"<<endl;
+    for(int i = 0; i<N; i++){
+        cout<<reinas[i]<<"|";
+    }
+
+    cout<<endl<<endl;
+
+    //Asignamos el valor del arreglo hacia el tablero
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            tablero[reinas[i]][j] = 1;
+        }
+    }
+
+    cout<<"\nMostrando el tablero"<<endl;
+    for(int i = 0; i<N; i++){
+        for(int j = 0; j<N; j++){
             cout<<tablero[i][j]<<"|";
         }
         cout<<endl;
     }
-}
 
-void saltoCaballo(int i, int x, int y, bool &exito)
-{
-    int nx, ny;
-    int k = 0; // inicializa contador de posibles 8 movimientos
-    exito = false;
-
-    do
-    {
-        k++;
-        nx = x + d[k - 1][0];
-        ny = y + d[k - 1][1];
-
-        // Determina si nuevas coordenadas son aceptables
-        if ((nx >= 0) && (nx < N) && (ny >= 0) && (ny < N) && (tablero[nx][ny] == 0))
-        {
-            // Anota moviemiento
-            tablero[nx][ny] = i;
-            escribirTablero(); // funcion para mostrar el tablero (matriz)
-
-            if (i < N * N)
-            {
-                saltoCaballo(i + 1, nx, ny, exito);
-
-                if (!exito)
-                {
-                    tablero[nx][ny] = 0; // no se alcanzo la solucion, se borra anotacion
-                }
-            }
-            else
-            {
-                exito = true; // caballo a cubierto el tablero
-            }
-        }
-    } while ((k < 8) && !exito);
-}
-int main(){
-    bool exito;
-    int fila=1;
-    int columna=0;
-
-    tablero[fila][columna] = 1;
-
-    saltoCaballo(2, fila, columna, exito);
-
-    if(exito){ //Si exito es verdadero, hemos encontrado un camino
-        cout<<"Camino encontrado"<<endl; 
-        escribirTablero();
-    }
-    else{
-        cout<<"No hay camino"<<endl;
-    }
-
-    return 0;
+    cout<<endl<<endl;
 }
